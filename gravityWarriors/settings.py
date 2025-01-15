e3c22d3d8f928a1573ba2f3caa4c7041701c945b
Krystian Wierciak <krystianwierciak@MacBook-Pro-von-Krystian.local>
2025-01-15 17:08:30 +0100

    Edit settings.py file to add templates , media , and static directories.

--- gravityWarriors/settings.py
+++ gravityWarriors/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'main',
+    'scoreboard',
     'django_bootstrap5',
 ]
 
@@ -120,7 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static']
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
