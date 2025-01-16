3bdf764c9c5ea1d576caa1c95cfa4b0fca7c0952
Krystian Wierciak <krystianwierciak@MacBook-Pro-von-Krystian.local>
2025-01-16 17:46:41 +0100

    Extending urls.py to new_Dips/ and new_Pull_ups/ for pull ups form.

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import scoreboard
+from .views import scoreboard_list, new_Dips , new_Pull_ups
 
 urlpatterns = [
-    path('', scoreboard, name='scoreboard'),
+    path('', scoreboard_list, name='scoreboard_list'),
+    path('new_Dips/', new_Dips, name='new_Dips'),
+    path('new_Pull_ups/', new_Pull_ups, name='new_Pull_ups'),
+
 ]
